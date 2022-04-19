69838bcd47339be819b0513ee2e4eb121a84d174
sahulkodalimac@icloud.com <sahulkodali@icloud.com>
2022-04-18 23:01:36 -0400

    validation without message

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -4,7 +4,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
   form: FormGroup;
@@ -12,20 +12,28 @@ export class LoginComponent implements OnInit {
   userId = '';
   password = '';
   hide = true;
+
   constructor(private fb: FormBuilder) {
     this.form = this.fb.group({
-      userId: ['', Validators.required],
-      password: ['', Validators.required]
-  });
+      userId: [
+        null,
+        Validators.compose([Validators.required, Validators.email]),
+      ],
+      password: [
+        null,
+        Validators.compose([
+          Validators.required,
+          Validators.min(10),
+          Validators.max(12),
+        ]),
+      ],
+    });
   }
 
-  ngOnInit(): void {
-  }
-
-
+  ngOnInit(): void {}
 
   captureLoginData() {
-    console.log('data:: ');
+    const val = this.form.value;
+    console.log('data:: ', val.userId, val.password);
   }
-
 }
